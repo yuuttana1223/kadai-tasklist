6a3699bb578ea9d5e5ba7e083ef54c1646aec484
Yuu Tanaka <yutanadreamfilel.seed@gmail.com>
2020-08-21 06:55:22 +0000

    CRUD tasks

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>ID: {{ $task->id }} のタスク編集ページ</h1>
+
+  <div class="row">
+    <div class="col-6">
+      {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+      <div class="form-group">
+        {!! Form::label('content', 'タスク内容:') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+      </div>
+      {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
+
+      {!! Form::close() !!}
+    </div>
+  </div>
+
+
+
+  @endsection
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>タスク一覧</h1>
+
+@if (count($tasks) > 0)
+<table class="table tabel-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>タスク内容</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($tasks as $task)
+    <tr>
+      <td>{{ $task->id }}</td>
+      <td>{!! link_to_route('tasks.show', $task->content, ['task' => $task->id]) !!}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+@endif
+
+{!! link_to_route('tasks.create', '新規登録', [], ['class' => 'btn btn-primary']) !!}
+
+@endsection
