b8971ddac1d99b952a8bae16625f79de3efbd305
Yuu Tanaka <yutanadreamfilel.seed@gmail.com>
2020-08-26 06:38:17 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+	return view('welcome');
+});
+// Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
